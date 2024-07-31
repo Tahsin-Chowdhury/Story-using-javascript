f4b2294519b5f034e14c4e9ed6b0c97324248210
Tahsin Chowdhury <ctahsin573@gmail.com>
2024-07-31 17:55:03 +0000

    story

---
+++ script.js
@@ -0,0 +1,18 @@
+let adjective = "Old";
+
+let number = "25";
+
+let myNumber = 8**2
+myNumber = myNumber -20
+console.log(myNumber)
+
+boolean = false
+let rainyday = false
+console.log(rainyday)
+
+
+let userName= prompt("enter your name")
+
+console.log("A old grandpa who works as security claims he has seen a ghost nearby the area he lives in. However the locals don't believe him and consider him to be a fool untill the ghost comes to annoy everyone in the neighbourhood.")
+
+console.log( userName + adjective + number + myNumber + rainyday)
